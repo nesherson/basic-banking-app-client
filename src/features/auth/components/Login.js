8f167ea1f93b3ac4fc821124f852b30509b1c456
nesherson <nesad.omeragich27@gmail.com>
2021-09-02 02:25:01 +0200

    Add login page

---
+++ src/features/auth/components/Login.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+import LoginForm from './LoginForm';
+import Logo from './Logo';
+import img from '../../../assets/images/loginImg.jpg';
+
+const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+`;
+
+const ImageWrapper = styled.section`
+  width: 1080px;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+`;
+
+const Image = styled.img`
+  height: 100%;
+  box-sizing: border-box;
+  max-width: 100%;
+  max-height: 100%;
+  width: 100%;
+  object-fit: cover;
+`;
+
+const FormWrapper = styled.section`
+  flex-grow: 1;
+  padding: 2.5em 7em;
+`;
+
+const Header = styled.h1`
+  margin-bottom: 2.75em;
+  margin-top: 2.95em;
+  color: #1e3e60;
+  font-size: 1.55rem;
+`;
+
+const SubHeader = styled.h3`
+  font-weight: 400;
+  color: #1e3e60;
+`;
+
+const LoginDescription = styled.p`
+  font-size: 0.9rem;
+  color: #667d94;
+`;
+
+const Link = styled(NavLink)`
+  text-decoration: none;
+  color: #0070ff;
+`;
+
+const Text = styled.p`
+  margin: 1.5em 0;
+  font-size: 0.98rem;
+  color: #1e3e60;
+`;
+
+function Login() {
+  return (
+    <Container>
+      <ImageWrapper>
+        <Image src={img} />
+      </ImageWrapper>
+      <FormWrapper>
+        <Logo />
+        <Header>Login</Header>
+        <SubHeader>Login to your account</SubHeader>
+        <LoginDescription>
+          Thank you for getting back to Basic Bank, let's sign you in.
+        </LoginDescription>
+        <LoginForm />
+        <Text>
+          Don't have an account yet? <Link to='#'>Join Basic Bank</Link>
+        </Text>
+      </FormWrapper>
+    </Container>
+  );
+}
+
+export default Login;
---
+++ src/features/auth/components/LoginForm.js
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
+
+import { EMAIL_REGEXP } from '../../../constants/constants';
+
+import { EmailInput, PasswordInput } from './Forms';
+
+const FormWrapper = styled.div``;
+
+const Form = styled.form`
+  max-width: 420px;
+  min-width: 340px;
+`;
+
+const FormColumn = styled.div`
+  display: flex;
+  width: 100%;
+  margin-top: 1.25em;
+`;
+
+const FormControl = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const Button = styled.button`
+  padding: 0.8em 2.25em;
+  color: #fff;
+  font-size: 0.93rem;
+  background-color: #0070ff;
+  border: 1px solid #0070ff;
+  align-self: flex-start;
+  box-sizing: border-box;
+`;
+
+function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <FormWrapper>
+      <Form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <FormColumn>
+          <FormControl>
+            <EmailInput
+              label='Email'
+              name='email'
+              register={register}
+              required
+              pattern={EMAIL_REGEXP}
+              errors={errors}
+            />
+          </FormControl>
+        </FormColumn>
+        <FormColumn>
+          <FormControl>
+            <PasswordInput
+              label='Password'
+              name='password'
+              register={register}
+              required
+              minLength={6}
+              errors={errors}
+            />
+          </FormControl>
+        </FormColumn>
+        <FormColumn>
+          <FormControl>
+            <Button type='submit'>Create Account</Button>
+          </FormControl>
+        </FormColumn>
+      </Form>
+    </FormWrapper>
+  );
+}
+
+export default LoginForm;
