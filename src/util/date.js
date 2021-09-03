2362da88053f8306c669b92cb1f92a4687bb4e7c
nesherson <nesad.omeragich27@gmail.com>
2021-09-04 00:47:06 +0200

    Add date helper function

---
+++ src/util/date.js
@@ -0,0 +1,7 @@
+export function getLocalDateWithOffset() {
+  let localDateWithOffset = new Date();
+  localDateWithOffset = new Date(
+    localDateWithOffset - localDateWithOffset.getTimezoneOffset() * 60000
+  );
+  return localDateWithOffset;
+}
