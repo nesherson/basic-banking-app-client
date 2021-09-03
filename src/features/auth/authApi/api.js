cca20048b71e4c9c572f85c70cfd7d0d991cbb15
nesherson <nesad.omeragich27@gmail.com>
2021-09-03 23:55:52 +0200

    Export login and signup functions into separate file

---
+++ src/features/auth/authApi/api.js
@@ -0,0 +1,62 @@
+async function postSignupUser({
+  firstName,
+  lastName,
+  email,
+  country,
+  password,
+}) {
+  try {
+    const response = await fetch('http://localhost:5000/auth/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        email,
+        country,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return data;
+    }
+
+    throw new Error(data.error);
+  } catch (error) {
+    return error;
+  }
+}
+
+async function postLoginUser({ email, password }) {
+  try {
+    const response = await fetch('http://localhost:5000/auth/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return data;
+    }
+
+    throw new Error(data.error);
+  } catch (error) {
+    return error;
+  }
+}
+
+export { postSignupUser, postLoginUser };
