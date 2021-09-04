0ce5676fc6ee61496ee1ac3fc7dc19b611652e1a
nesherson <nesad.omeragich27@gmail.com>
2021-09-04 23:39:56 +0200

    Add user and transactios section to dashboard

---
+++ src/features/user/components/dashboard/transactionsSection/TransactionsSection.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.section`
+  flex: 1;
+  background-color: #c2c2c2;
+`;
+
+function TransactionsSection() {
+  return <Container />;
+}
+
+export default TransactionsSection;
---
+++ src/features/user/components/dashboard/userSection/UserSection.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.section`
+  width: 920px;
+  background-color: #ccc;
+`;
+
+function UserSection() {
+  return <Container />;
+}
+
+export default UserSection;
