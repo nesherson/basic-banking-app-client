4421311171ff42559aedb046fe66966cca9dc8bf
nesherson <nesad.omeragich27@gmail.com>
2021-09-05 04:23:13 +0200

    Add transactions section to dashboard

---
+++ src/features/user/components/dashboard/transactionsSection/Transaction.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+import {
+  ChevronsUp,
+  ChevronsDown,
+  ChevronsRight,
+  ChevronsLeft,
+} from 'react-feather';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0.2em 3.5em;
+  height: 100px;
+`;
+
+const Time = styled.span`
+  color: #7b8c9d;
+  font-size: 0.95rem;
+  margin-right: 1.75em;
+`;
+
+const Icon = styled.div`
+  border-radius: 50%;
+  background-color: #fbfbfb;
+  color: #404040;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px;
+  margin: 2px 1.75em 2px 0;
+  width: calc(64px * 0.45);
+  height: calc(64px * 0.45);
+  border: 1px solid rgba(204, 204, 204, 0.3);
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TransactionType = styled.span`
+  color: #161a1d;
+  font-size: 1rem;
+  text-transform: uppercase;
+`;
+
+const TransactionDescription = styled.p`
+  margin: 0;
+  font-size: 0.85rem;
+  color: #7b8c9d;
+`;
+
+const TransactionAmount = styled.span`
+  color: #161a1d;
+  font-size: 1rem;
+  margin-left: auto;
+`;
+
+function Transaction({ time, type, description, amount }) {
+  let typeIcon;
+
+  if (type === 'deposit') typeIcon = <ChevronsDown />;
+  else if (type === 'withdraw') typeIcon = <ChevronsDown />;
+  else typeIcon = <ChevronsRight />;
+
+  return (
+    <Container>
+      <Time>{time}</Time>
+      <Icon>{typeIcon}</Icon>
+      <Wrapper>
+        <TransactionType>{type}</TransactionType>
+        {type === 'payment' ? (
+          <TransactionDescription>{description}</TransactionDescription>
+        ) : null}
+      </Wrapper>
+      <TransactionAmount>{amount}KM</TransactionAmount>
+    </Container>
+  );
+}
+
+export default Transaction;
--- src/features/user/components/dashboard/transactionsSection/TransactionsSection.js
+++ src/features/user/components/dashboard/transactionsSection/TransactionsSection.js
@@ -1,12 +1,86 @@
 import styled from 'styled-components';
+import { Search } from 'react-feather';
+
+import Transaction from './Transaction';
 
 const Container = styled.section`
   flex: 1;
-  background-color: #c2c2c2;
+  background-color: #fff;
+  box-sizing: border-box;
+`;
+
+const Transactions = styled.div`
+  //padding: 2em 3em;
+`;
+
+const TransactionDetails = styled.div`
+  padding: 2em 3em;
+`;
+
+const Header = styled.header`
+  margin-bottom: 2.5em;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2em 3.5em 0 3.5em;
+`;
+
+const Headline = styled.h3`
+  padding: 0;
+  margin: 0;
+  font-size: 1.3rem;
+  font-weight: 600;
+  color: #161a1d;
+`;
+
+const Icon = styled.div``;
+
+const Date = styled.p`
+  font-size: 0.95rem;
+  color: #7b8c9d;
+  padding: 0 3.5em;
+`;
+
+const TransactionList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
 `;
 
 function TransactionsSection() {
-  return <Container />;
+  return (
+    <Container>
+      <Transactions>
+        <Header>
+          <Headline>Latest Transactions</Headline>
+          <Icon>
+            <Search size={20} />
+          </Icon>
+        </Header>
+        <Date>Mon, Mar 1</Date>
+        <TransactionList>
+          <Transaction
+            time={'16:47'}
+            type={'deposit'}
+            description={''}
+            amount={570.0}
+          />
+          <Transaction
+            time={'17:47'}
+            type={'withdraw'}
+            description={''}
+            amount={70.0}
+          />
+          <Transaction
+            time={'18:47'}
+            type={'payment'}
+            description={'Have fun.'}
+            amount={150.0}
+          />
+        </TransactionList>
+      </Transactions>
+    </Container>
+  );
 }
 
 export default TransactionsSection;
