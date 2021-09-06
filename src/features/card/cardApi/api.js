f9023e9457e14aeb2d708ef1406ea835a60bfc5e
nesherson <nesad.omeragich27@gmail.com>
2021-09-06 04:46:20 +0200

    Add function for fetching card data by user id

---
+++ src/features/card/cardApi/api.js
@@ -0,0 +1,24 @@
+async function fetchCardDataByUsedId({ id, token }) {
+  try {
+    const response = await fetch(`http://localhost:5000/cards/user/${id}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return data;
+    }
+
+    throw new Error(data.error);
+  } catch (error) {
+    return error;
+  }
+}
+
+export { fetchCardDataByUsedId };
