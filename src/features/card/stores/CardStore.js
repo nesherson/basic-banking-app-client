ed559717f5fcc38d4d7476fa9fc27cc992f02c65
nesherson <nesad.omeragich27@gmail.com>
2021-09-06 04:47:33 +0200

    Add card state store

---
+++ src/features/card/stores/CardStore.js
@@ -0,0 +1,65 @@
+import { makeAutoObservable, runInAction } from 'mobx';
+import { createContext } from 'react';
+
+import { fetchCardDataByUsedId } from '../cardApi/api';
+
+class CardStore {
+  card = {
+    id: null,
+    cardNumber: '',
+    type: '',
+    network: '',
+    balance: null,
+    userId: null,
+  };
+  fetchCardDataStatus = {
+    isFetching: false,
+    isSuccess: false,
+    isError: false,
+    errorMessage: '',
+  };
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  clearFetchCardDataStatus() {
+    runInAction(() => {
+      this.fetchCardDataStatus.isFetching = false;
+      this.fetchCardDataStatus.isSuccess = false;
+      this.fetchCardDataStatus.isError = false;
+      this.fetchCardDataStatus.errorMessage = '';
+    });
+  }
+
+  async getCardDataByUsedId(id) {
+    this.fetchCardDataStatus.isFetching = true;
+    try {
+      const response = await fetchCardDataByUsedId(id);
+      if (response instanceof Error) {
+        throw new Error(response.message);
+      }
+      runInAction(() => {
+        this.card.id = response.id;
+        this.card.cardNumber = response.cardNumber;
+        this.card.type = response.type;
+        this.card.network = response.network;
+        this.card.balance = response.balance;
+        this.card.userId = response.userId;
+
+        this.fetchCardDataStatus.isFetching = false;
+        this.fetchCardDataStatus.isSuccess = true;
+        this.fetchCardDataStatus.isError = false;
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.fetchCardDataStatus.isFetching = false;
+        this.fetchCardDataStatus.isSuccess = false;
+        this.fetchCardDataStatus.isError = true;
+        this.fetchCardDataStatus.errorMessage = error.message;
+      });
+    }
+  }
+}
+
+export const CardStoreContext = createContext(new CardStore());
