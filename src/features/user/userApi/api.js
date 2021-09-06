dddb312c6577198b6c5337be7222d159f3b88e53
nesherson <nesad.omeragich27@gmail.com>
2021-09-06 04:46:52 +0200

    Add function for fetching user data

---
+++ src/features/user/userApi/api.js
@@ -0,0 +1,24 @@
+async function fetchUserData({ id, token }) {
+  try {
+    const response = await fetch(`http://localhost:5000/users/${id}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return data;
+    }
+
+    throw new Error(data.error);
+  } catch (error) {
+    return error;
+  }
+}
+
+export { fetchUserData };
