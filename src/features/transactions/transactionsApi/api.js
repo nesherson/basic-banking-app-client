ee04c3170918bd54f9c09e94d64b696680c4297e
nesherson <nesad.omeragich27@gmail.com>
2021-09-07 16:59:10 +0200

    Add function for fetching latest transactions

---
+++ src/features/transactions/transactionsApi/api.js
@@ -0,0 +1,27 @@
+async function fetchLatestTransactions({ cardId, token }) {
+  try {
+    const response = await fetch(
+      `http://localhost:5000/transactions/card/${cardId}/latest?resultLimit=4`,
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return data;
+    }
+
+    throw new Error(data.error);
+  } catch (error) {
+    return error;
+  }
+}
+
+export { fetchLatestTransactions };
