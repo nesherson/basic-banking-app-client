aad6ca44d6c3ff43457cec1b73cddc981c484e83
nesherson <nesad.omeragich27@gmail.com>
2021-09-13 03:25:06 +0200

    Add list header

---
+++ src/features/user/components/transactions/listHeader/ListHeader.js
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  grid-column: 1 / 2;
+  grid-row: 3 / 4;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 0 3.5em;
+  border-bottom: 2px solid #161a1d;
+  margin: 0 3.5em;
+`;
+
+const Date = styled.span`
+  display: flex;
+  justify-content: flex-start;
+  flex: 1;
+  font-size: 0.95rem;
+  font-weight: 600;
+  color: #627384;
+  border-right: 1px solid rgba(33, 38, 44, 0.2);
+`;
+
+const Method = styled.span`
+  display: flex;
+  justify-content: flex-start;
+  flex: 1;
+  padding-left: 1em;
+  font-size: 0.95rem;
+  font-weight: 600;
+  color: #627384;
+  border-right: 1px solid rgba(33, 38, 44, 0.2);
+`;
+
+const Users = styled.span`
+  display: flex;
+  justify-content: flex-start;
+  flex: 3;
+  padding-left: 1em;
+  font-size: 0.95rem;
+  font-weight: 600;
+  color: #627384;
+  border-right: 1px solid rgba(33, 38, 44, 0.2);
+`;
+
+const Description = styled.span`
+  display: flex;
+  justify-content: flex-start;
+  flex: 3;
+  padding-left: 1em;
+  font-size: 0.95rem;
+  font-weight: 600;
+  color: #627384;
+  border-right: 1px solid rgba(33, 38, 44, 0.2);
+`;
+
+const Amount = styled.span`
+  display: flex;
+  justify-content: flex-end;
+  flex: 2;
+  font-size: 0.95rem;
+  font-weight: 600;
+  color: #627384;
+  text-transform: uppercase;
+`;
+
+function ListHeader() {
+  return (
+    <Container>
+      <Date>Date</Date>
+      <Method>Method</Method>
+      <Users>Users</Users>
+      <Description>Description</Description>
+      <Amount>Amount</Amount>
+    </Container>
+  );
+}
+
+export default ListHeader;
