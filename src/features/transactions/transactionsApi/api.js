ed60d1da6913bd05de6921545135c9327fbd9a21
nesherson <nesad.omeragich27@gmail.com>
2021-09-15 17:44:42 +0200

    Add fuction for fetching last month transactions

--- src/features/transactions/transactionsApi/api.js
+++ src/features/transactions/transactionsApi/api.js
@@ -24,4 +24,30 @@ async function fetchLatestTransactions({ cardId, token }) {
   }
 }
 
-export { fetchLatestTransactions };
+async function fetchLastMonthTransactions({ cardId, token }) {
+  try {
+    const response = await fetch(
+      `http://localhost:5000/transactions/card/${cardId}/last-month`,
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return data;
+    }
+
+    throw new Error(data.error);
+  } catch (error) {
+    return error;
+  }
+}
+
+export { fetchLatestTransactions, fetchLastMonthTransactions };
