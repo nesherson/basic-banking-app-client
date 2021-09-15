1f8cdaa6d63b4b4b51aaadbab72d23f50e532748
nesherson <nesad.omeragich27@gmail.com>
2021-09-15 17:42:00 +0200

    Add routes for dashboard and transactions page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
 import Signup from './features/auth/components/Signup';
 import Login from './features/auth/components/Login';
 import Layout from './features/user/components/layout/Layout';
 import Dashboard from './features/user/components/dashboard/Dashboard';
+import Transactions from './features/user/components/transactions/Transactions';
 
 function App() {
   return (
     <Router>
-      <Route path='/signup' component={Signup} />
-      <Route path='/login' component={Login} />
+      <Route exact path='/signup' component={Signup} />
+      <Route exact path='/login' component={Login} />
       <Route path='/'>
         <Layout>
-          <Dashboard />
+          <Switch>
+            <Route path='/dashboard' component={Dashboard} />
+            <Route path='/transactions' component={Transactions} />
+            <Redirect to='/dashboard' />
+          </Switch>
         </Layout>
       </Route>
     </Router>
