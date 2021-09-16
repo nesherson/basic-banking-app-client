d1d6578c28fcd55a0ea0f2e06308187dc3bb8c7c
nesherson <nesad.omeragich27@gmail.com>
2021-09-16 23:31:19 +0200

    Add helper function for parsing date string to locale datetime

--- src/util/date.js
+++ src/util/date.js
@@ -1,7 +1,18 @@
-export function getLocalDateWithOffset() {
+function getLocalDateWithOffset() {
   let localDateWithOffset = new Date();
   localDateWithOffset = new Date(
     localDateWithOffset - localDateWithOffset.getTimezoneOffset() * 60000
   );
   return localDateWithOffset;
 }
+
+function parseStrDateToLocaleDate(str) {
+  return new Date(str).toLocaleDateString('en-GB', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+}
+
+export { getLocalDateWithOffset, parseStrDateToLocaleDate };
