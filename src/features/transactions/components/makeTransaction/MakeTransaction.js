68b179e48080589bb9f1aefd0bed9e2608cf5586
nesherson <nesad.omeragich27@gmail.com>
2021-09-17 05:12:28 +0200

    Add component for making deposit or withdraw

---
+++ src/features/transactions/components/makeTransaction/MakeTransaction.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.section`
+  grid-column: 1 / 2;
+  grid-row: 3 / 4;
+`;
+
+function MakeTransaction() {
+  return <Container>asd</Container>;
+}
+
+export default MakeTransaction;
